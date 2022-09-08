8611f4a26eb1e96b8042f9477ebd55cf63f7b4b6
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2022-09-08 19:48:51 -0400

    reduce width for primary and secondary buttons

--- components/shared/styles.tsx
+++ components/shared/styles.tsx
@@ -16,7 +16,7 @@ export const LandingSection = styled(motion.div)`
 export const SecondaryButton = styled(motion.div)`
   display: flex;
   font-size: 1em;
-  max-width: 360px;
+  max-width: 336px;
   min-width: 360px;
   height: 104px;
   color: black;
@@ -43,7 +43,7 @@ export const SecondaryButton = styled(motion.div)`
 export const PrimaryButton = styled(motion.div)`
   display: flex;
   font-size: 1em;
-  max-width: 360px;
+  max-width: 336px;
   min-width: 360px;
   text-align: center;
   justify-content: center;
