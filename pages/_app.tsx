e514d75ce6412e530920be4f47b89a888f4d5f07
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2022-10-16 01:44:17 -0400

    CHORE | Refactor Google analytics into component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,10 @@
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { sendPageViewAnalytics } from "../lib/google_analytics";
-import Script from "next/script";
+import GoogleAnalytics from "../lib/google_analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.events.on("routeChangeComplete", sendPageViewAnalytics);
-
-    return () => {
-      router.events.off("routeChangeComplete", sendPageViewAnalytics);
-    };
-  }, [router.events]);
-
   return (
     <>
-      <Script
-        id="gtag-base"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-          `,
-        }}
-      />
+      <GoogleAnalytics />
       <Component {...pageProps} />;
     </>
   );
