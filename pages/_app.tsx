f42f375702634d47cf37d91f35125ef379df1dbf
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2022-10-16 13:50:56 -0400

    improve global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import GoogleAnalytics from "../lib/google_analytics";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
