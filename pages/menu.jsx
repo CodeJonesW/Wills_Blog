9e5cb0641bfe29145c4c30b4b8132e8dab12dff0
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2022-11-20 15:39:35 -0500

    jot form menu page

---
+++ pages/menu.jsx
@@ -0,0 +1,10 @@
+// create functional component
+import JotformEmbed from "react-jotform-embed";
+
+export default function Menu() {
+  return (
+    <div style={{ width: "100vw" }}>
+      <JotformEmbed src="https://form.jotform.com/223235466649160" />{" "}
+    </div>
+  );
+}
