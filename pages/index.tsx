423419fa94c883d11109428b4b7c798299a32a4f
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-11-13 01:12:58 -0600

    remote default api hello file

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,6 @@ import { Box, Typography } from "@mui/material";
 import { UnshrinkableDiv } from "../components/shared/styles";
 import Blog from "./blog";
 
-const PostsContainer = styled(motion.div)`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-  padding-top: 24px;
-`;
-
 interface BlogProps {
   posts: post[];
 }
