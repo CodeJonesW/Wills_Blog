2964d075c349d25b16f5c992bf1e1dbc1f0ad4c7
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-11-14 19:34:05 -0600

    profile wording update

--- pages/index.tsx
+++ pages/index.tsx
@@ -132,7 +132,7 @@ export default function Home({ posts }: BlogProps) {
                 }}
                 color="text.primary"
               >
-                {`Im the backend developer who also makes clean user interfaces.
+                {`Im a backend developer who also makes clean user interfaces.
                 
                 A few technologies I've been working with recently:
                 - React
